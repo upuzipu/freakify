bc3b9f7e5d2e66bbce788d307bdc46414e2b9e3d
upuzipu <egornovikov52@gmail.com>
2024-01-03 15:53:27 +0300

    changed read.me

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function MainPage(props) {
+
+    return (
+        <div>
+            <h1>Всем при вет</h1>
+        </div>
+
+    )
+}
+
+export default MainPage;
+
